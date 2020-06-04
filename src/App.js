ad7e9f43f62183567297008a39157d6c5fbcdd4e
OusmaneMballo <ousmanemballo576@gmail.com>
2020-06-04 12:21:04 +0000

    Creation des compenent

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './Welcom';
+import Welcom from './Welcom';
 
 function App() {
   return (
-    <div className="App">
+    
+    
+      <Welcom/>
+    /*<div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Gestion de Stock
         </a>
       </header>
-    </div>
+    </div>*/
   );
 }
 
---
+++ src/Welcom.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+class Welcom extends React.Component {
+    render(){
+        return (
+            <h1>Welcom</h1>
+        );
+    }
+       
+    
+}
+export default Welcom;
